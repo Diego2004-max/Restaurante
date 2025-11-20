64d3bd7a38ad0a3238f80a7d5337515dc2cad319
Diego Alejandro Mallama Yandun <diego.mallamay@campusucc.edu.co>
2025-11-19 23:24:54 -0500

    act templates

--- core/forms.py
+++ core/forms.py
@@ -1,15 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    ROLE_CHOICES = [
-        ("cliente", "Cliente"),
-        ("mesero", "Mesero"),
-    ]
+ROLE_CHOICES = [
+    ("admin", "Administrador"),
+    ("mesero", "Mesero"),
+    ("cliente", "Cliente"),
+]
 
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
     role = forms.ChoiceField(choices=ROLE_CHOICES)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2", "role"]
+        fields = ["username", "email", "password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get("password")
+        p2 = cleaned_data.get("confirm_password")
+
+        if p1 != p2:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
